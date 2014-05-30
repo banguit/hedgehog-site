8792c128dbbf2bf0ba59b33e6261ae659c0ed728
Dmitry Antonenko <banguit@gmail.com>
2014-05-30 22:37:25 +0300

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+/**
+ * @fileOverview Main class for application, that used as original entry point
+ */
+
+goog.provide('hedgehog.Application');
+
+hedgehog.Application.initialize = function() {
+    console.log('Here we go!');
+};
