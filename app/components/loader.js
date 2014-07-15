67a09b7ae7d8b47f01ef00988a44096926a41111
Dmitry Antonenko <banguit@gmail.com>
2014-07-15 11:17:20 +0300

    Update loader settings

--- app/components/loader.js
+++ app/components/loader.js
@@ -31,8 +31,8 @@ hedgehog.Loader.prototype.createDom = function() {
         color: '#fff', // #rgb or #rrggbb or array of colors
         speed: 0.8, // Rounds per second
         trail: 23, // Afterglow percentage
-        shadow: true, // Whether to render a shadow
-        hwaccel: false, // Whether to use hardware acceleration
+        shadow: false, // Whether to render a shadow
+        hwaccel: true, // Whether to use hardware acceleration
         className: 'spinner', // The CSS class to assign to the spinner
         zIndex: 2e9, // The z-index (defaults to 2000000000)
         top: '50%', // Top position relative to parent
