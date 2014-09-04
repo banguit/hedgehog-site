5ac30d7686b17e5d72596b5bde62550780d9d7c5
Dmitry Antonenko <banguit@gmail.com>
2014-09-05 01:13:12 +0300

    About page texts update

--- app/app.js
+++ app/app.js
@@ -18,24 +18,81 @@ goog.require('hedgehog.filters.ComponentsInitializationApplicationFilter');
  * Initializes the application
  */
 hedgehog.start = function() {
-    // -- Application initialization -- //
-    var app = new hedgehog.core.Application();
 
-    // -- Register routes -- //
-    // Default route should be wrapped in {.. route definition ..}
-    app.mapRoute('{/}{/blog{/}{/:action{/}}{/:id{/}}}', hedgehog.controllers.BlogController); // Default route
-    app.mapRoute('/projects{/}', hedgehog.controllers.ProjectsController);
-    app.mapRoute('/about{/}{/:action{/}}[?*]', hedgehog.controllers.AboutController);
-    // TODO: 404
+    hedgehog.bindReady_(function() {
+        // -- Application initialization -- //
+        var app = new hedgehog.core.Application();
 
-    // -- Register application filters -- //
-    app.addApplicationFilter(new hedgehog.filters.ComponentsInitializationApplicationFilter());
+        // -- Register routes -- //
+        // Default route should be wrapped in {.. route definition ..}
+        app.mapRoute('{/}{/blog{/}{/:action{/}}{/:id{/}}}', hedgehog.controllers.BlogController); // Default route
+        app.mapRoute('/projects{/}', hedgehog.controllers.ProjectsController);
+        app.mapRoute('/about{/}{/:action{/}}[?*]', hedgehog.controllers.AboutController);
+        // TODO: 404
 
-    // -- Register action filters -- //
-    app.addActionFilter(new hedgehog.filters.ComponentsInitializationActionFilter(), null, 0);
+        // -- Register application filters -- //
+        app.addApplicationFilter(new hedgehog.filters.ComponentsInitializationApplicationFilter());
 
-    // Execute application
-    app.run();
+        // -- Register action filters -- //
+        app.addActionFilter(new hedgehog.filters.ComponentsInitializationActionFilter(), null, 0);
+
+        // Execute application
+        app.run();
+    });
 };
+
+/**
+ * @param {Function} handler
+ * @private
+ */
+hedgehog.bindReady_ = function(handler) {
+
+    var called = false;
+
+    function ready() {
+        if (called) { return; }
+        called = true;
+        handler();
+    };
+
+    function tryScroll(){
+        if (called) { return; }
+        try {
+            document.documentElement.doScroll("left");
+            ready();
+        } catch(e) {
+            setTimeout(tryScroll, 10);
+        }
+    };
+
+    if ( document.addEventListener ) { // native event
+        document.addEventListener( "DOMContentLoaded", ready, false);
+    } else if ( document.attachEvent ) {  // IE
+
+        try {
+            var isFrame = window.frameElement != null;
+        } catch(e) {}
+
+        // IE, the document is not inside a frame
+        if ( document.documentElement.doScroll && !isFrame ) {
+            tryScroll();
+        }
+
+        // IE, the document is inside a frame
+        document.attachEvent("onreadystatechange", function(){
+            if ( document.readyState === "complete" ) {
+                ready();
+            }
+        })
+    }
+
+    // Old browsers
+    if (window.addEventListener)
+        window.addEventListener('load', ready, false);
+    else if (window.attachEvent) {
+        window.attachEvent('onload', ready);
+    }
+};
+
 
 goog.exportSymbol('app.start', hedgehog.start);
